1c4fdc72cb5d23d1f0942052efb38c0158bd38eb
knakamura <knakamura@mis.doshisha.ac.jp>
2016-06-08 18:14:20 +0900

    disc.jsの追記

--- scripts/disc.js
+++ scripts/disc.js
@@ -9,7 +9,56 @@ var BT = React.createClass({
   }
 });
 
+var songdata;
+$(() => {
+    $.ajaxSetup({ async: false });
+    $.getJSON("data.json",(data) => {songdata = data;});
+    console.log(songdata);
+    $.ajaxSetup({ async: true });
+})
+
+var SongContainer = React.createClass({
+    render: () => {
+        var SongList = songdata.map((isong) => {
+            return (
+                <Song data = {isong}></Song>
+            );
+        });        
+        return(
+            <div className = "container">
+                <ul className="list-group">
+                    <li className="list-group-item row">
+                        <div className="col-xs-6">TITLE</div>
+                        <div className="col-xs-4">SINGER</div>
+                        <div className="col-xs-1">SEASON</div>
+                    </li>
+                </ul>
+                <ul className="list-group">
+                    {SongList}
+                </ul>
+            </div>
+        );
+    }
+});
+
+var Song = React.createClass({
+    render: function() {
+        var tstyle = {
+            color: "white"
+        };
+        return (
+            <li className="list-group-item row" style={tstyle}>
+                <div className="col-xs-6">{this.props.data.song_title}</div>
+                <div className="col-xs-4">{this.props.data.singer.join(",")}</div>
+                <div className="col-xs-1">{this.props.data.series}</div>
+            </li>
+        );
+    }
+});
+
 React.render(
-  <BT />,
-  document.getElementById('ikusaba-inner')
+    <div>
+        <SongContainer></SongContainer>
+    </div>,
+    document.getElementById('ikusaba-inner')
 );
