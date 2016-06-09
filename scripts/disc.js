3825db5cf3ae4e0231c0ab06259fe09a63e66c72
RauziDssh <rauzi.eee@gmail.com>
2016-06-09 19:06:49 +0900

    fix

--- scripts/disc.js
+++ scripts/disc.js
@@ -1,54 +1,109 @@
 var songdata;
+var selectedsongdata;
 
 $(() => {
-    $.ajaxSetup({ async: false });
-    $.getJSON("data.json",(data) => {songdata = data;});
-    console.log(songdata);
-    $.ajaxSetup({ async: true });
+    $.ajaxSetup({
+        async: false
+    });
+    $.getJSON("data.json", (data) => {
+        songdata = data;
+    });
+    $.ajaxSetup({
+        async: true
+    });
+    selectedsongdata = songdata;
 })
 
-var SongContainer = React.createClass({
-    render: () => {
-        var SongList = songdata.map((isong) => {
-            return (
-                <Song data = {isong}></Song>
-            );
-        });        
-        return(
-            <div className = "container">
-                <ul className="list-group">
-                    <li className="list-group-item row">
-                        <div className="col-xs-6">TITLE</div>
-                        <div className="col-xs-4">SINGER</div>
-                        <div className="col-xs-1">SEASON</div>
-                    </li>
-                </ul>
-                <ul className="list-group">
-                    {SongList}
-                </ul>
-            </div>
-        );
-    }
-});
-
 var Song = React.createClass({
-    render: function() {
+    render: function () {
         var tstyle = {
             color: "white"
         };
-        return (
-            <li className="list-group-item row" style={tstyle}>
-                <div className="col-xs-6">{this.props.data.song_title}</div>
-                <div className="col-xs-4">{this.props.data.singer.join(",")}</div>
-                <div className="col-xs-1">{this.props.data.series}</div>
-            </li>
-        );
-    }
+        return ( < li className = "list-group-item row"
+            style = {
+                tstyle
+            } >
+            < div className = "col-xs-6" > {
+                this.props.data.song_title
+            } < /div> < div className = "col-xs-4" > {
+            this.props.data.singer.join(",")
+        } < /div> < div className = "col-xs-1" > {
+        this.props.data.series
+    } < /div> < /li >
+);
+}
 });
 
-React.render(
-    <div>
-        <SongContainer></SongContainer>
-    </div>,
-    document.getElementById('ikusaba-inner')
-);
+var SongContainer = React.createClass({
+            render: function () {
+                var SongList = this.props.sdata.map((isong) => {
+                        return ( < Song data = {
+                                isong
+                            } > < /Song>);
+                        });
+                    return ( < ul className = "list-group" > {
+                            SongList
+                        } < /ul>);
+                    }
+                });
+var rendering = function () {
+                    React.render( < div >
+                        < SongContainer sdata = {
+                            selectedsongdata
+                        } > < /SongContainer> < /div > 
+                        , document.getElementById('songlist')
+                    );
+                }
+rendering();
+
+var song_singer  = songdata.map((s) => {return s.id});
+var song_season  = songdata.map((s) => {return s.id});
+                    
+$("#seasonselect").change(function () {
+                var selected_season = $("#seasonselect").val();
+                if (selected_season == "ALL") {
+                    song_season = songdata.map((s) => {return s.id});
+                } else {
+                    song_season = songdata.filter((song, index) => {
+                        var season = song.series;
+                        if (season == selected_season) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }).map((s) => {return s.id});
+                }
+    console.log(song_season);
+    SetSongData();
+});
+                    
+$("#singerselect").change(function () {
+                
+                var selected_singer = $("#singerselect").val();
+                if (selected_singer == "ALL") {
+                    song_singer = songdata.map((s) => {return s.id});
+                } else {
+                song_singer = songdata.filter((song, index) => {
+                var singers = song.singer;
+                if ($.inArray(selected_singer, singers) > -1 || selected_singer == singers) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }).map((s) => {return s.id});
+        }
+    console.log(song_singer);
+    SetSongData();
+});
+                    
+var SetSongData = () => {
+    selectedsongdata = songdata.filter((s,index) => {
+        if($.inArray(s.id, song_singer) > -1 && $.inArray(s.id, song_season) > -1){
+            return true;
+        }else{
+            return false;
+        }
+    });
+    //console.log(selectedsongdata);
+    rendering();
+}
