ebd6fc6c34b4d465dfac23b93aaeb1db75ca177a
RauziDssh <bmn1070@mail4.doshisha.ac.jp>
2016-06-10 13:37:36 +0900

    index.htmlの差し替え
    disc.jsの変更

--- scripts/disc.js
+++ scripts/disc.js
@@ -1,6 +1,10 @@
 var songdata;
 var selectedsongdata;
 
+Array.prototype.selectMany = function (fn) {
+    return this.map(fn).reduce(function (x, y) { return x.concat(y); }, []);
+};
+
 $(() => {
     $.ajaxSetup({
         async: false
@@ -12,6 +16,19 @@ $(() => {
         async: true
     });
     selectedsongdata = songdata;
+    var series = songdata.map((s) => s.series).filter(function (x, i, self) {
+        return self.indexOf(x) === i;
+    });
+    var singer = songdata.selectMany((x) => x.singer).filter(function (x, i, self) {
+        return self.indexOf(x) === i;
+    });
+    
+    series.forEach((e) => {
+        $("#seasonselect").append("<option value="+e+">"+e+"</option>");
+    });
+    singer.forEach((e) => {
+        $("#singerselect").append("<option value="+e+">"+e+"</option>");
+    });
 })
 
 var Song = React.createClass({
@@ -34,6 +51,7 @@ var Song = React.createClass({
 }
 });
 
+
 var SongContainer = React.createClass({
             render: function () {
                 var SongList = this.props.sdata.map((isong) => {
@@ -46,23 +64,30 @@ var SongContainer = React.createClass({
                         } < /ul>);
                     }
                 });
-var rendering = function () {
-                    React.render( < div >
-                        < SongContainer sdata = {
-                            selectedsongdata
-                        } > < /SongContainer> < /div > 
-                        , document.getElementById('songlist')
-                    );
-                }
-rendering();
 
-var song_singer  = songdata.map((s) => {return s.id});
-var song_season  = songdata.map((s) => {return s.id});
-                    
-$("#seasonselect").change(function () {
+            var rendering = function () {
+                React.render( < div >
+                    < SongContainer sdata = {
+                        selectedsongdata
+                    } > < /SongContainer> < /div > 
+                    , document.getElementById('songlist')
+                );
+            }
+            rendering();
+
+            var song_singer = songdata.map((s) => {
+                return s.id
+            });
+            var song_season = songdata.map((s) => {
+                return s.id
+            });
+
+            $("#seasonselect").change(function () {
                 var selected_season = $("#seasonselect").val();
                 if (selected_season == "ALL") {
-                    song_season = songdata.map((s) => {return s.id});
+                    song_season = songdata.map((s) => {
+                        return s.id
+                    });
                 } else {
                     song_season = songdata.filter((song, index) => {
                         var season = song.series;
@@ -71,39 +96,42 @@ $("#seasonselect").change(function () {
                         } else {
                             return false;
                         }
-                    }).map((s) => {return s.id});
+                    }).map((s) => {
+                        return s.id
+                    });
                 }
-    console.log(song_season);
-    SetSongData();
-});
-                    
-$("#singerselect").change(function () {
-                
+                SetSongData();
+            });
+
+            $("#singerselect").change(function () {
+
                 var selected_singer = $("#singerselect").val();
                 if (selected_singer == "ALL") {
-                    song_singer = songdata.map((s) => {return s.id});
+                    song_singer = songdata.map((s) => {
+                        return s.id
+                    });
                 } else {
-                song_singer = songdata.filter((song, index) => {
-                var singers = song.singer;
-                if ($.inArray(selected_singer, singers) > -1 || selected_singer == singers) {
-                    return true;
-                } else {
-                    return false;
+                    song_singer = songdata.filter((song, index) => {
+                        var singers = song.singer;
+                        if ($.inArray(selected_singer, singers) > -1 || selected_singer == singers) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }).map((s) => {
+                        return s.id
+                    });
                 }
-            }).map((s) => {return s.id});
-        }
-    console.log(song_singer);
-    SetSongData();
-});
-                    
-var SetSongData = () => {
-    selectedsongdata = songdata.filter((s,index) => {
-        if($.inArray(s.id, song_singer) > -1 && $.inArray(s.id, song_season) > -1){
-            return true;
-        }else{
-            return false;
-        }
-    });
-    //console.log(selectedsongdata);
-    rendering();
-}
+                SetSongData();
+            });
+
+            var SetSongData = () => {
+                selectedsongdata = songdata.filter((s, index) => {
+                    if ($.inArray(s.id, song_singer) > -1 && $.inArray(s.id, song_season) > -1) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+                rendering();
+            }
